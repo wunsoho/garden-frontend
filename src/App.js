f6a62276279733249304f79a249f71284b4ca085
garden <wunsoho@naver.com>
2024-01-04 18:58:29 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import React, { useEffect } from "react";
 import './App.css';
+import Test from "./component/test"
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   function setScreenSize() {
@@ -11,24 +12,16 @@ function App() {
     setScreenSize();
   });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div>
+      <Routes>
+       <Route path="/" element={<Test/>}/>
+      </Routes>
     </div>
-  );
-}
+    </BrowserRouter>
+  )
+  }
+
 
 
 export default App;
